111a94a160d45ca0f61ad29fb414eed73670c13d
ClementTsang <clementjhtsang@gmail.com>
2019-09-07 00:49:15 -0400

    Started using heim.  Not working yet.

--- src/widgets/cpu.rs
+++ src/widgets/cpu.rs
@@ -1,22 +1,32 @@
 use sysinfo::{ProcessorExt, System, SystemExt};
 
-pub struct TimedCPUData {
+pub struct CPUData {
 	pub cpu_name : Box<str>,
 	pub cpu_usage : u32,
+}
+
+pub struct TimedCPUPackagesStruct {
+	pub processor_list : Vec<CPUData>,
 	pub time : std::time::SystemTime,
 }
 
-pub fn get_cpu_data_list(sys : &System) -> Vec<TimedCPUData> {
+pub fn get_cpu_data_list(sys : &System) -> TimedCPUPackagesStruct {
 	let cpu_data = sys.get_processor_list();
 	let mut cpu_vec = Vec::new();
 
 	for cpu in cpu_data {
-		cpu_vec.push(TimedCPUData {
+		cpu_vec.push(CPUData {
 			cpu_name : Box::from(cpu.get_name()),
 			cpu_usage : (cpu.get_cpu_usage() * 100_f32).ceil() as u32,
-			time : std::time::SystemTime::now(),
 		})
 	}
 
-	cpu_vec
+	TimedCPUPackagesStruct {
+		processor_list : cpu_vec,
+		time : std::time::SystemTime::now(),
+	}
+}
+
+pub fn clear_old_cpu_data() -> bool {
+	true
 }
--- src/widgets/disks.rs
+++ src/widgets/disks.rs
@@ -1,24 +1,53 @@
-use sysinfo::{System, SystemExt, Disk, DiskExt};
+use heim_common::prelude::StreamExt;
 
-pub struct DiskInfo<'a> {
-	pub name: &'a str,
-	pub mount_point: &'a str,
-	pub avail_space: u64,
-	pub total_space: u64,
+pub struct DiskInfo {
+	pub name : Box<str>,
+	pub mount_point : Box<str>,
+	pub avail_space : u64,
+	pub total_space : u64,
 }
 
-pub fn get_disk_usage_list(sys: &System) -> Vec<DiskInfo> {
-	let result_disks = sys.get_disks();
-	let mut vec_disks : Vec<DiskInfo> = Vec::new();
+pub struct IOInfo {
+	pub name : Box<str>,
+	pub read_bytes : u64,
+	pub write_bytes : u64,
+}
 
-	for disk in result_disks {
-		vec_disks.push(DiskInfo {
-			name: disk.get_name().to_str().unwrap(),
-			mount_point: disk.get_mount_point().to_str().unwrap(),
-			avail_space: disk.get_available_space(),
-			total_space: disk.get_total_space(),
-		});
+pub async fn get_io_usage_list() -> Result<Vec<IOInfo>, heim::Error> {
+	let mut io_list : Vec<IOInfo> = Vec::new();
+	let mut counters = heim::disk::io_counters();
+	while let Some(counter) = counters.next().await {
+		dbg!(counter?);
 	}
 
-	vec_disks
+	println!("\n\n--- Per physical disk ---\n");
+
+	let mut counters = heim::disk::io_counters_physical();
+	while let Some(counter) = counters.next().await {
+		dbg!(counter?);
+	}
+
+	Ok(io_list)
+}
+
+pub async fn get_disk_usage_list() -> Result<Vec<DiskInfo>, heim::Error> {
+	let mut vec_disks : Vec<DiskInfo> = Vec::new();
+	let mut partitions_stream = heim::disk::partitions_physical();
+
+	while let Some(part) = partitions_stream.next().await {
+		let part = part?;
+		let usage = heim::disk::usage(part.mount_point().to_path_buf()).await?;
+
+		println!(
+			"{:<17} {:<10} {:<10} {:<10} {:<10} {}",
+			part.device().unwrap().to_str().unwrap(),
+			usage.total().get::<heim_common::units::information::megabyte>(),
+			usage.used().get::<heim_common::units::information::megabyte>(),
+			usage.free().get::<heim_common::units::information::megabyte>(),
+			part.file_system().as_str(),
+			part.mount_point().to_string_lossy(),
+		);
+	}
+
+	Ok(vec_disks)
 }
