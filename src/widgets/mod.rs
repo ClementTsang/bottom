471209f511f9a23edf35529cf04cd6fa23e1b408
ClementTsang <clementjhtsang@gmail.com>
2019-09-08 19:56:23 -0400

    Refactored code such that it fits more of tui-rs' example.

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,6 +1,101 @@
+pub mod cpu;
 pub mod disks;
-pub mod temperature;
+pub mod mem;
 pub mod network;
 pub mod processes;
-pub mod mem;
-pub mod cpu;
+pub mod temperature;
+
+use sysinfo::{System, SystemExt};
+
+#[derive(Default)]
+pub struct App<'a> {
+	pub should_quit : bool,
+	pub list_of_cpu_packages : Vec<cpu::CPUData>,
+	pub list_of_io : Vec<disks::IOData>,
+	pub list_of_physical_io : Vec<disks::IOData>,
+	pub memory : mem::MemData,
+	pub swap : mem::MemData,
+	pub list_of_temperature : Vec<temperature::TempData>,
+	pub network : network::NetworkData,
+	pub list_of_processes : Vec<processes::ProcessData>,
+	pub list_of_disks : Vec<disks::DiskData>,
+	pub title : &'a str,
+}
+
+fn set_if_valid<T : std::clone::Clone>(result : &Result<T, heim::Error>, value_to_set : &mut T) {
+	if let Ok(result) = result {
+		*value_to_set = (*result).clone();
+	}
+}
+
+impl<'a> App<'a> {
+	pub fn new(title : &str) -> App {
+		let mut app = App::default();
+		app.title = title;
+		app
+	}
+
+	pub fn on_key(&mut self, c : char) {
+		match c {
+			'q' => self.should_quit = true,
+			_ => {}
+		}
+	}
+
+	pub async fn update_data(&mut self) {
+		// Initialize
+		let mut sys = System::new();
+
+		sys.refresh_system();
+		sys.refresh_network();
+
+		// What we want to do: For timed data, if there is an error, just do not add.  For other data, just don't update!
+		set_if_valid(&network::get_network_data(&sys), &mut self.network);
+		set_if_valid(&cpu::get_cpu_data_list(&sys), &mut self.list_of_cpu_packages);
+
+		// TODO: Joining all futures would be better...
+		set_if_valid(&processes::get_sorted_processes_list(processes::ProcessSorting::NAME, false).await, &mut self.list_of_processes);
+		set_if_valid(&disks::get_disk_usage_list().await, &mut self.list_of_disks);
+		set_if_valid(&disks::get_io_usage_list(false).await, &mut self.list_of_io);
+		set_if_valid(&disks::get_io_usage_list(true).await, &mut self.list_of_physical_io);
+		set_if_valid(&mem::get_mem_data_list().await, &mut self.memory);
+		set_if_valid(&mem::get_swap_data_list().await, &mut self.swap);
+		set_if_valid(&temperature::get_temperature_data().await, &mut self.list_of_temperature);
+
+		/*
+		// DEBUG - output results
+		for process in &list_of_processes {
+			println!(
+				"Process: {} with PID {}, CPU: {}%, MEM: {} MB",
+				process.command, process.pid, process.cpu_usage_percent, process.mem_usage_in_mb,
+			);
+		}
+		for disk in &list_of_disks {
+			println!("{} is mounted on {}: {} used.", disk.name, disk.mount_point, disk.used_space as f64 / disk.total_space as f64);
+			// TODO: Check if this is valid
+		}
+
+		for io in &list_of_io {
+			println!("IO counter for {}: {} writes, {} reads.", &io.mount_point, io.write_bytes, io.read_bytes);
+		}
+
+		for io in &list_of_physical_io {
+			println!("Physical IO counter for {}: {} writes, {} reads.", &io.mount_point, io.write_bytes, io.read_bytes);
+		}
+
+		for cpu in &list_of_cpu_packages {
+			println!("CPU {} has {}% usage!", &cpu.cpu_name, cpu.cpu_usage);
+		}
+
+		println!("Memory usage: {} out of {} is used", memory.mem_used, memory.mem_total);
+
+		println!("Memory usage: {} out of {} is used", swap.mem_used, swap.mem_total);
+
+		for sensor in &list_of_temperature {
+			println!("Sensor for {} is at {} degrees Celsius", sensor.component_name, sensor.temperature);
+		}
+
+		println!("Network: {} rx, {} tx", network.rx, network.tx);
+		*/
+	}
+}
--- src/widgets/processes.rs
+++ src/widgets/processes.rs
@@ -13,7 +13,7 @@ pub enum ProcessSorting {
 
 // Possible process info struct?
 #[derive(Clone, Default)]
-pub struct ProcessInfo {
+pub struct ProcessData {
 	pub pid : u32,
 	pub cpu_usage_percent : f32,
 	pub mem_usage_in_mb : u64,
@@ -50,10 +50,10 @@ async fn cpu_usage(process : heim::process::Process) -> heim::process::ProcessRe
 	Ok((process, usage_2 - usage_1))
 }
 
-pub async fn get_sorted_processes_list(sorting_method : ProcessSorting, reverse_order : bool) -> Result<Vec<ProcessInfo>, heim::Error> {
+pub async fn get_sorted_processes_list(sorting_method : ProcessSorting, reverse_order : bool) -> Result<Vec<ProcessData>, heim::Error> {
 	let mut process_stream = heim::process::processes().map_ok(cpu_usage).try_buffer_unordered(std::usize::MAX);
 
-	let mut process_vector : Vec<ProcessInfo> = Vec::new();
+	let mut process_vector : Vec<ProcessData> = Vec::new();
 	while let Some(process) = process_stream.next().await {
 		if let Ok(process) = process {
 			let (process, cpu_usage) = process;
@@ -69,7 +69,7 @@ pub async fn get_sorted_processes_list(sorting_method : ProcessSorting, reverse_
 				});
 				*/
 
-				process_vector.push(ProcessInfo {
+				process_vector.push(ProcessData {
 					command : process.name().await.unwrap_or_else(|_| "".to_string()),
 					pid : process.pid() as u32,
 					cpu_usage_percent : cpu_usage.get::<units::ratio::percent>(),
@@ -83,7 +83,7 @@ pub async fn get_sorted_processes_list(sorting_method : ProcessSorting, reverse_
 	Ok(process_vector)
 }
 
-pub fn sort_processes(sorting_method : ProcessSorting, process_vector : &mut Vec<ProcessInfo>, reverse_order : bool) {
+pub fn sort_processes(sorting_method : ProcessSorting, process_vector : &mut Vec<ProcessData>, reverse_order : bool) {
 	match sorting_method {
 		ProcessSorting::CPU => process_vector.sort_by(|a, b| get_ordering(a.cpu_usage_percent, b.cpu_usage_percent, reverse_order)),
 		ProcessSorting::MEM => process_vector.sort_by(|a, b| get_ordering(a.mem_usage_in_mb, b.mem_usage_in_mb, reverse_order)),
