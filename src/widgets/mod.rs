5a0faf40cb9381ef1c076cd5ef86dadb374a7039
ClementTsang <clementjhtsang@gmail.com>
2019-09-09 18:34:13 -0400

    Ironed out as many kinks as possible in terms of smoothness.

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -7,8 +7,23 @@ pub mod temperature;
 
 use sysinfo::{System, SystemExt};
 
+#[allow(dead_code)]
 pub struct App<'a> {
+	title : &'a str,
 	pub should_quit : bool,
+	pub process_sorting_type : processes::ProcessSorting,
+	pub process_sorting_reverse : bool,
+	pub to_be_resorted : bool,
+}
+
+fn set_if_valid<T : std::clone::Clone>(result : &Result<T, heim::Error>, value_to_set : &mut T) {
+	if let Ok(result) = result {
+		*value_to_set = (*result).clone();
+	}
+}
+
+#[derive(Default, Clone)]
+pub struct Data {
 	pub list_of_cpu_packages : Vec<cpu::CPUData>,
 	pub list_of_io : Vec<disks::IOData>,
 	pub list_of_physical_io : Vec<disks::IOData>,
@@ -18,15 +33,39 @@ pub struct App<'a> {
 	pub network : network::NetworkData,
 	pub list_of_processes : Vec<processes::ProcessData>,
 	pub list_of_disks : Vec<disks::DiskData>,
-	pub title : &'a str,
-	process_sorting_type : processes::ProcessSorting,
-	process_sorting_reverse : bool,
+}
+
+pub struct DataState {
+	pub data : Data,
 	sys : System,
 }
 
-fn set_if_valid<T : std::clone::Clone>(result : &Result<T, heim::Error>, value_to_set : &mut T) {
-	if let Ok(result) = result {
-		*value_to_set = (*result).clone();
+impl Default for DataState {
+	fn default() -> Self {
+		DataState {
+			data : Data::default(),
+			sys : System::new(),
+		}
+	}
+}
+
+impl DataState {
+	pub async fn update_data(&mut self) {
+		self.sys.refresh_system();
+		self.sys.refresh_network();
+
+		// What we want to do: For timed data, if there is an error, just do not add.  For other data, just don't update!
+		set_if_valid(&network::get_network_data(&self.sys), &mut self.data.network);
+		set_if_valid(&cpu::get_cpu_data_list(&self.sys), &mut self.data.list_of_cpu_packages);
+
+		// TODO: We can convert this to a multi-threaded task...
+		set_if_valid(&processes::get_sorted_processes_list().await, &mut self.data.list_of_processes);
+		set_if_valid(&disks::get_disk_usage_list().await, &mut self.data.list_of_disks);
+		set_if_valid(&disks::get_io_usage_list(false).await, &mut self.data.list_of_io);
+		set_if_valid(&disks::get_io_usage_list(true).await, &mut self.data.list_of_physical_io);
+		set_if_valid(&mem::get_mem_data_list().await, &mut self.data.memory);
+		set_if_valid(&mem::get_swap_data_list().await, &mut self.data.swap);
+		set_if_valid(&temperature::get_temperature_data().await, &mut self.data.list_of_temperature);
 	}
 }
 
@@ -35,18 +74,9 @@ impl<'a> App<'a> {
 		App {
 			title,
 			process_sorting_type : processes::ProcessSorting::NAME, // TODO: Change this based on input args...
-			sys : System::new(),                                    // TODO: Evaluate whether this will cause efficiency issues...
-			list_of_cpu_packages : Vec::new(),
-			list_of_disks : Vec::new(),
-			list_of_physical_io : Vec::new(),
-			list_of_io : Vec::new(),
-			list_of_processes : Vec::new(),
-			list_of_temperature : Vec::new(),
-			network : network::NetworkData::default(),
-			memory : mem::MemData::default(),
-			swap : mem::MemData::default(),
 			should_quit : false,
 			process_sorting_reverse : false,
+			to_be_resorted : false,
 		}
 	}
 
@@ -54,48 +84,38 @@ impl<'a> App<'a> {
 		match c {
 			'q' => self.should_quit = true,
 			'c' => {
-				self.process_sorting_type = processes::ProcessSorting::CPU;
-				//processes::sort_processes(&self.process_sorting_type, &mut self.list_of_processes, self.process_sorting_reverse);
-				// TODO: This CANNOT run while it is updating...
+				self.process_sorting_type = processes::ProcessSorting::CPU; // TODO: Change this such that reversing can be done by just hitting "c" twice...
+				self.to_be_resorted = true;
 			}
 			'm' => {
 				self.process_sorting_type = processes::ProcessSorting::MEM;
-				//processes::sort_processes(&self.process_sorting_type, &mut self.list_of_processes, self.process_sorting_reverse);
+				self.to_be_resorted = true;
 			}
 			'p' => {
 				self.process_sorting_type = processes::ProcessSorting::PID;
-				//processes::sort_processes(&self.process_sorting_type, &mut self.list_of_processes, self.process_sorting_reverse);
+				self.to_be_resorted = true;
 			}
 			'n' => {
 				self.process_sorting_type = processes::ProcessSorting::NAME;
-				//processes::sort_processes(&self.process_sorting_type, &mut self.list_of_processes, self.process_sorting_reverse);
+				self.to_be_resorted = true;
 			}
 			'r' => {
 				self.process_sorting_reverse = !self.process_sorting_reverse;
-				//processes::sort_processes(&self.process_sorting_type, &mut self.list_of_processes, self.process_sorting_reverse);
+				self.to_be_resorted = true;
 			}
 			_ => {}
 		}
 	}
 
-	pub async fn update_data(&mut self) {
-		self.sys.refresh_system();
-		self.sys.refresh_network();
+	pub fn on_left(&mut self) {
+	}
 
-		// What we want to do: For timed data, if there is an error, just do not add.  For other data, just don't update!
-		set_if_valid(&network::get_network_data(&self.sys), &mut self.network);
-		set_if_valid(&cpu::get_cpu_data_list(&self.sys), &mut self.list_of_cpu_packages);
+	pub fn on_right(&mut self) {
+	}
 
-		// TODO: Joining all futures would be better...
-		set_if_valid(
-			&processes::get_sorted_processes_list(&self.process_sorting_type, self.process_sorting_reverse).await,
-			&mut self.list_of_processes,
-		);
-		set_if_valid(&disks::get_disk_usage_list().await, &mut self.list_of_disks);
-		set_if_valid(&disks::get_io_usage_list(false).await, &mut self.list_of_io);
-		set_if_valid(&disks::get_io_usage_list(true).await, &mut self.list_of_physical_io);
-		set_if_valid(&mem::get_mem_data_list().await, &mut self.memory);
-		set_if_valid(&mem::get_swap_data_list().await, &mut self.swap);
-		set_if_valid(&temperature::get_temperature_data().await, &mut self.list_of_temperature);
+	pub fn on_up(&mut self) {
+	}
+
+	pub fn on_down(&mut self) {
 	}
 }
