8c7b9dab35f12336c392cfaa0f687c3a03db6421
ClementTsang <clementjhtsang@gmail.com>
2019-09-09 19:31:03 -0400

    I can't spell.

--- src/widgets/processes.rs
+++ src/widgets/processes.rs
@@ -45,7 +45,7 @@ fn get_ordering<T : std::cmp::PartialOrd>(a_val : T, b_val : T, reverse_order : 
 
 async fn cpu_usage(process : heim::process::Process) -> heim::process::ProcessResult<(heim::process::Process, heim_common::units::Ratio)> {
 	let usage_1 = process.cpu_usage().await?;
-	futures_timer::Delay::new(std::time::Duration::from_millis(100)).await?;
+	futures_timer::Delay::new(std::time::Duration::from_millis(150)).await?;
 	let usage_2 = process.cpu_usage().await?;
 
 	Ok((process, usage_2 - usage_1))
