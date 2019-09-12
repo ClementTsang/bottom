03bda12c5a12fb83c09e1b668c75a89ab4060e5b
ClementTsang <clementjhtsang@gmail.com>
2019-09-11 23:34:26 -0400

    More refactoring; added logging and error files.

---
+++ src/utils/logging.rs
@@ -0,0 +1,17 @@
+pub fn init_logger() -> Result<(), fern::InitError> {
+	fern::Dispatch::new()
+		.format(|out, message, record| {
+			out.finish(format_args!(
+				"{}[{}][{}] {}",
+				chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+				record.target(),
+				record.level(),
+				message
+			))
+		})
+		.level(if cfg!(debug_assertions) { log::LevelFilter::Debug } else { log::LevelFilter::Info })
+		.chain(fern::log_file("debug.log")?)
+		.apply()?;
+
+	Ok(())
+}
