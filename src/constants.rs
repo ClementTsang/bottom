dc725f296ce940cba4f8a03b83344780405db68f
ClementTsang <clementjhtsang@gmail.com>
2019-09-25 12:42:39 -0400

    Added const to make rate switching in the future easier.

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,4 @@
 // TODO: Store like three minutes of data, then change how much is shown based on scaling!
 pub const STALE_MAX_MILLISECONDS : u64 = 60 * 1000; // We wish to store at most 60 seconds worth of data.  This may change in the future, or be configurable.
 pub const TICK_RATE_IN_MILLISECONDS : u64 = 200; // We use this as it's a good value to work with.
+pub const DEFAULT_REFRESH_RATE_IN_MILLISECONDS : u128 = 1000;
