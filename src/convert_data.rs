3151d93aec24efe86f2d9e12b4e1643a90271659
ClementTsang <clementjhtsang@gmail.com>
2019-12-06 00:57:04 -0500

    Update tui version... legends aren't showing up yet, will have to fork again.

--- src/convert_data.rs
+++ src/convert_data.rs
@@ -1,13 +1,12 @@
 use crate::{app::data_collection, constants};
 use constants::*;
 
-pub fn update_temp_row(app_data : &data_collection::Data, temp_type : &data_collection::temperature::TemperatureType) -> Vec<Vec<String>> {
-	let mut sensor_vector : Vec<Vec<String>> = Vec::new();
+pub fn update_temp_row(app_data: &data_collection::Data, temp_type: &data_collection::temperature::TemperatureType) -> Vec<Vec<String>> {
+	let mut sensor_vector: Vec<Vec<String>> = Vec::new();
 
 	if (&app_data.list_of_temperature_sensor).is_empty() {
 		sensor_vector.push(vec!["No Sensors Found".to_string(), "".to_string()])
-	}
-	else {
+	} else {
 		for sensor in &app_data.list_of_temperature_sensor {
 			sensor_vector.push(vec![
 				sensor.component_name.to_string(),
@@ -24,8 +23,8 @@ pub fn update_temp_row(app_data : &data_collection::Data, temp_type : &data_coll
 	sensor_vector
 }
 
-pub fn update_disk_row(app_data : &data_collection::Data) -> Vec<Vec<String>> {
-	let mut disk_vector : Vec<Vec<String>> = Vec::new();
+pub fn update_disk_row(app_data: &data_collection::Data) -> Vec<Vec<String>> {
+	let mut disk_vector: Vec<Vec<String>> = Vec::new();
 	for disk in &app_data.list_of_disks {
 		let io_activity = if app_data.list_of_io.len() > 2 {
 			if let Some(io_package) = &app_data.list_of_io.last() {
@@ -44,37 +43,29 @@ pub fn update_disk_row(app_data : &data_collection::Data) -> Vec<Vec<String>> {
 						(
 							if read_bytes_per_sec < 1024 {
 								format!("{}B", read_bytes_per_sec)
-							}
-							else if read_bytes_per_sec < 1024 * 1024 {
+							} else if read_bytes_per_sec < 1024 * 1024 {
 								format!("{}KB", read_bytes_per_sec / 1024)
-							}
-							else {
+							} else {
 								format!("{}MB", read_bytes_per_sec / 1024 / 1024)
 							},
 							if write_bytes_per_sec < 1024 {
 								format!("{}B", write_bytes_per_sec)
-							}
-							else if write_bytes_per_sec < 1024 * 1024 {
+							} else if write_bytes_per_sec < 1024 * 1024 {
 								format!("{}KB", write_bytes_per_sec / 1024)
-							}
-							else {
+							} else {
 								format!("{}MB", write_bytes_per_sec / 1024 / 1024)
 							},
 						)
-					}
-					else {
+					} else {
 						("0B".to_string(), "0B".to_string())
 					}
-				}
-				else {
+				} else {
 					("0B".to_string(), "0B".to_string())
 				}
-			}
-			else {
+			} else {
 				("0B".to_string(), "0B".to_string())
 			}
-		}
-		else {
+		} else {
 			("0B".to_string(), "0B".to_string())
 		};
 		disk_vector.push(vec![
@@ -83,14 +74,12 @@ pub fn update_disk_row(app_data : &data_collection::Data) -> Vec<Vec<String>> {
 			format!("{:.0}%", disk.used_space as f64 / disk.total_space as f64 * 100_f64),
 			if disk.free_space < 1024 {
 				disk.free_space.to_string() + "MB"
-			}
-			else {
+			} else {
 				(disk.free_space / 1024).to_string() + "GB"
 			},
 			if disk.total_space < 1024 {
 				disk.total_space.to_string() + "MB"
-			}
-			else {
+			} else {
 				(disk.total_space / 1024).to_string() + "GB"
 			},
 			io_activity.0,
@@ -101,8 +90,8 @@ pub fn update_disk_row(app_data : &data_collection::Data) -> Vec<Vec<String>> {
 	disk_vector
 }
 
-pub fn update_process_row(app_data : &data_collection::Data) -> Vec<Vec<String>> {
-	let mut process_vector : Vec<Vec<String>> = Vec::new();
+pub fn update_process_row(app_data: &data_collection::Data) -> Vec<Vec<String>> {
+	let mut process_vector: Vec<Vec<String>> = Vec::new();
 
 	for process in &app_data.list_of_processes {
 		process_vector.push(vec![
@@ -113,16 +102,13 @@ pub fn update_process_row(app_data : &data_collection::Data) -> Vec<Vec<String>>
 				"{:.1}%",
 				if let Some(mem_usage) = process.mem_usage_percent {
 					mem_usage
-				}
-				else if let Some(mem_usage_kb) = process.mem_usage_kb {
+				} else if let Some(mem_usage_kb) = process.mem_usage_kb {
 					if let Some(mem_data) = app_data.memory.last() {
 						(mem_usage_kb / 1024) as f64 / mem_data.mem_total_in_mb as f64 * 100_f64
-					}
-					else {
+					} else {
 						0_f64
 					}
-				}
-				else {
+				} else {
 					0_f64
 				}
 			),
@@ -132,14 +118,14 @@ pub fn update_process_row(app_data : &data_collection::Data) -> Vec<Vec<String>>
 	process_vector
 }
 
-pub fn update_cpu_data_points(show_avg_cpu : bool, app_data : &data_collection::Data) -> Vec<(String, Vec<(f64, f64)>)> {
-	let mut cpu_data_vector : Vec<(String, Vec<(f64, f64)>)> = Vec::new();
-	let mut cpu_collection : Vec<Vec<(f64, f64)>> = Vec::new();
+pub fn update_cpu_data_points(show_avg_cpu: bool, app_data: &data_collection::Data) -> Vec<(String, Vec<(f64, f64)>)> {
+	let mut cpu_data_vector: Vec<(String, Vec<(f64, f64)>)> = Vec::new();
+	let mut cpu_collection: Vec<Vec<(f64, f64)>> = Vec::new();
 
 	if !app_data.list_of_cpu_packages.is_empty() {
 		// I'm sorry for the if statement but I couldn't be bothered here...
 		for cpu_num in (if show_avg_cpu { 0 } else { 1 })..app_data.list_of_cpu_packages.last().unwrap().cpu_vec.len() {
-			let mut this_cpu_data : Vec<(f64, f64)> = Vec::new();
+			let mut this_cpu_data: Vec<(f64, f64)> = Vec::new();
 
 			for data in &app_data.list_of_cpu_packages {
 				let current_time = std::time::Instant::now();
@@ -186,24 +172,24 @@ pub fn update_cpu_data_points(show_avg_cpu : bool, app_data : &data_collection::
 	cpu_data_vector
 }
 
-pub fn update_mem_data_points(app_data : &data_collection::Data) -> Vec<(f64, f64)> {
+pub fn update_mem_data_points(app_data: &data_collection::Data) -> Vec<(f64, f64)> {
 	convert_mem_data(&app_data.memory)
 }
 
-pub fn update_swap_data_points(app_data : &data_collection::Data) -> Vec<(f64, f64)> {
+pub fn update_swap_data_points(app_data: &data_collection::Data) -> Vec<(f64, f64)> {
 	convert_mem_data(&app_data.swap)
 }
 
-pub fn update_mem_data_values(app_data : &data_collection::Data) -> Vec<(u64, u64)> {
-	let mut result : Vec<(u64, u64)> = Vec::new();
+pub fn update_mem_data_values(app_data: &data_collection::Data) -> Vec<(u64, u64)> {
+	let mut result: Vec<(u64, u64)> = Vec::new();
 	result.push(get_most_recent_mem_values(&app_data.memory));
 	result.push(get_most_recent_mem_values(&app_data.swap));
 
 	result
 }
 
-fn get_most_recent_mem_values(mem_data : &[data_collection::mem::MemData]) -> (u64, u64) {
-	let mut result : (u64, u64) = (0, 0);
+fn get_most_recent_mem_values(mem_data: &[data_collection::mem::MemData]) -> (u64, u64) {
+	let mut result: (u64, u64) = (0, 0);
 
 	if !mem_data.is_empty() {
 		if let Some(most_recent) = mem_data.last() {
@@ -215,8 +201,8 @@ fn get_most_recent_mem_values(mem_data : &[data_collection::mem::MemData]) -> (u
 	result
 }
 
-fn convert_mem_data(mem_data : &[data_collection::mem::MemData]) -> Vec<(f64, f64)> {
-	let mut result : Vec<(f64, f64)> = Vec::new();
+fn convert_mem_data(mem_data: &[data_collection::mem::MemData]) -> Vec<(f64, f64)> {
+	let mut result: Vec<(f64, f64)> = Vec::new();
 
 	for data in mem_data {
 		let current_time = std::time::Instant::now();
@@ -224,8 +210,7 @@ fn convert_mem_data(mem_data : &[data_collection::mem::MemData]) -> Vec<(f64, f6
 			((TIME_STARTS_FROM as f64 - current_time.duration_since(data.instant).as_millis() as f64) * 10_f64).floor(),
 			if data.mem_total_in_mb == 0 {
 				-1000.0
-			}
-			else {
+			} else {
 				data.mem_used_in_mb as f64 / data.mem_total_in_mb as f64 * 100_f64
 			},
 		);
@@ -248,19 +233,19 @@ fn convert_mem_data(mem_data : &[data_collection::mem::MemData]) -> Vec<(f64, f6
 }
 
 pub struct ConvertedNetworkData {
-	pub rx : Vec<(f64, f64)>,
-	pub tx : Vec<(f64, f64)>,
-	pub rx_display : String,
-	pub tx_display : String,
+	pub rx: Vec<(f64, f64)>,
+	pub tx: Vec<(f64, f64)>,
+	pub rx_display: String,
+	pub tx_display: String,
 }
 
-pub fn update_network_data_points(app_data : &data_collection::Data) -> ConvertedNetworkData {
+pub fn update_network_data_points(app_data: &data_collection::Data) -> ConvertedNetworkData {
 	convert_network_data_points(&app_data.network)
 }
 
-pub fn convert_network_data_points(network_data : &[data_collection::network::NetworkData]) -> ConvertedNetworkData {
-	let mut rx : Vec<(f64, f64)> = Vec::new();
-	let mut tx : Vec<(f64, f64)> = Vec::new();
+pub fn convert_network_data_points(network_data: &[data_collection::network::NetworkData]) -> ConvertedNetworkData {
+	let mut rx: Vec<(f64, f64)> = Vec::new();
+	let mut tx: Vec<(f64, f64)> = Vec::new();
 
 	for data in network_data {
 		let current_time = std::time::Instant::now();
@@ -303,18 +288,14 @@ pub fn convert_network_data_points(network_data : &[data_collection::network::Ne
 		let num_bytes = last_num_bytes_entry.rx;
 		if num_bytes < 1024 {
 			format!("RX: {:4} B", num_bytes).to_string()
-		}
-		else if num_bytes < (1024 * 1024) {
+		} else if num_bytes < (1024 * 1024) {
 			format!("RX: {:4}KB", num_bytes / 1024).to_string()
-		}
-		else if num_bytes < (1024 * 1024 * 1024) {
+		} else if num_bytes < (1024 * 1024 * 1024) {
 			format!("RX: {:4}MB", num_bytes / 1024 / 1024).to_string()
-		}
-		else {
+		} else {
 			format!("RX: {:4}GB", num_bytes / 1024 / 1024 / 1024).to_string()
 		}
-	}
-	else {
+	} else {
 		"0B".to_string()
 	};
 
@@ -322,18 +303,14 @@ pub fn convert_network_data_points(network_data : &[data_collection::network::Ne
 		let num_bytes = last_num_bytes_entry.tx;
 		if num_bytes < 1024 {
 			format!("TX: {:4} B", num_bytes).to_string()
-		}
-		else if num_bytes < (1024 * 1024) {
+		} else if num_bytes < (1024 * 1024) {
 			format!("TX: {:4}KB", num_bytes / 1024).to_string()
-		}
-		else if num_bytes < (1024 * 1024 * 1024) {
+		} else if num_bytes < (1024 * 1024 * 1024) {
 			format!("TX: {:4}MB", num_bytes / 1024 / 1024).to_string()
-		}
-		else {
+		} else {
 			format!("TX: {:4}GB", num_bytes / 1024 / 1024 / 1024).to_string()
 		}
-	}
-	else {
+	} else {
 		"0B".to_string()
 	};
 
