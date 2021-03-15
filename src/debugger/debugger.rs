3fb6cc44313ebaf9131291d6e16be131aecf7bd3
ClementTsang <cjhtsang@uwaterloo.ca>
2021-03-15 03:27:16 -0400

    Add missing files

---
+++ src/debugger/debugger.rs
@@ -0,0 +1,2 @@
+//! Debugger binary.  This isn't implemented yet; the idea for this is to make it easier to troubleshoot bug reports
+//! by providing a built-in debugger to help gather relevant information to narrow down the problem.
