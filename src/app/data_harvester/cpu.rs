ea3eb5a46b8318797a16430c545687d930f60764
ClementTsang <cjhtsang@uwaterloo.ca>
2021-05-08 20:17:13 -0400

    Add alternative cpu convert for non-linux

--- src/app/data_harvester/cpu.rs
+++ src/app/data_harvester/cpu.rs
@@ -15,22 +15,36 @@ pub async fn get_cpu_data_list(
     previous_average_cpu_time: &mut Option<(PastCpuWork, PastCpuTotal)>,
 ) -> crate::error::Result<CpuHarvest> {
     use futures::StreamExt;
+    #[cfg(target_os = "linux")]
     use heim::cpu::os::linux::CpuTimeExt;
     use std::collections::VecDeque;
 
     fn convert_cpu_times(cpu_time: &heim::cpu::CpuTime) -> (f64, f64) {
-        let working_time: f64 = (cpu_time.user()
-            + cpu_time.nice()
-            + cpu_time.system()
-            + cpu_time.irq()
-            + cpu_time.soft_irq()
-            + cpu_time.steal())
-        .get::<heim::units::time::second>();
-        (
-            working_time,
-            working_time
-                + (cpu_time.idle() + cpu_time.io_wait()).get::<heim::units::time::second>(),
-        )
+        #[cfg(not(target_os = "linux"))]
+        {
+            let working_time: f64 =
+                (cpu_time.user() + cpu_time.system()).get::<heim::units::time::second>();
+            (
+                working_time,
+                working_time
+                    + (cpu_time.idle() + cpu_time.io_wait()).get::<heim::units::time::second>(),
+            )
+        }
+        #[cfg(target_os = "linux")]
+        {
+            let working_time: f64 = (cpu_time.user()
+                + cpu_time.nice()
+                + cpu_time.system()
+                + cpu_time.irq()
+                + cpu_time.soft_irq()
+                + cpu_time.steal())
+            .get::<heim::units::time::second>();
+            (
+                working_time,
+                working_time
+                    + (cpu_time.idle() + cpu_time.io_wait()).get::<heim::units::time::second>(),
+            )
+        }
     }
 
     fn calculate_cpu_usage_percentage(
