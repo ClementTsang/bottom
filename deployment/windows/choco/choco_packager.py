15d40ee93d0d1501741c23815a2433905f23dbd1
ClementTsang <cjhtsang@uwaterloo.ca>
2022-02-02 22:31:39 -0500

    ci: disable 32-bit in choco generation
    Seems like the 32-bit binary triggers false positives on VirusTotal.
    Going to disable this for now.

--- deployment/windows/choco/choco_packager.py
+++ deployment/windows/choco/choco_packager.py
@@ -7,17 +7,15 @@ from string import Template
 import os
 
 args = sys.argv
-deployment_file_path_32 = args[1]
-deployment_file_path_64 = args[2]
-version = args[3]
-nuspec_template = args[4]
-ps1_template = args[5]
-generated_nuspec = args[6]
-generated_ps1 = args[7]
-generated_ps1_dir = args[8]
+deployment_file_path_64 = args[1]
+version = args[2]
+nuspec_template = args[3]
+ps1_template = args[4]
+generated_nuspec = args[5]
+generated_ps1 = args[6]
+generated_ps1_dir = args[7]
 
 print("Generating Chocolatey package for:")
-print("     32-bit: %s" % deployment_file_path_32)
 print("     64-bit: %s" % deployment_file_path_64)
 print("     VERSION: %s" % version)
 print("     NUSPEC TEMPLATE: %s" % nuspec_template)
@@ -26,14 +24,10 @@ print("     GENERATED NUSPEC: %s" % generated_nuspec)
 print("     GENERATED PS1: %s" % generated_ps1)
 print("     GENERATED PS1 DIR: %s" % generated_ps1_dir)
 
-with open(deployment_file_path_32, "rb") as deployment_file_32, open(
-    deployment_file_path_64, "rb"
-) as deployment_file_64:
-    hash_32 = hashlib.sha1(deployment_file_32.read()).hexdigest()
+with open(deployment_file_path_64, "rb") as deployment_file_64:
     hash_64 = hashlib.sha1(deployment_file_64.read()).hexdigest()
 
-    print("Generated 32 hash: %s" % str(hash_32))
-    print("Generated 64 hash: %s" % str(hash_64))
+    print("Generated hash for 64-bit program: %s" % str(hash_64))
 
     with open(nuspec_template, "r") as template_file:
         template = Template(template_file.read())
@@ -48,7 +42,7 @@ with open(deployment_file_path_32, "rb") as deployment_file_32, open(
     os.makedirs(generated_ps1_dir)
     with open(ps1_template, "r") as template_file:
         template = Template(template_file.read())
-        substitute = template.safe_substitute(version=version, hash_32=hash_32, hash_64=hash_64)
+        substitute = template.safe_substitute(version=version, hash_64=hash_64)
         print("\n================== Generated chocolatey-install file ==================\n")
         print(substitute)
         print("\n============================================================\n")
