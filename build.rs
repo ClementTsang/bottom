7e870afb6657a26c268f16d5bb4728c43a21cb5a
ClementTsang <clementjhtsang@gmail.com>
2022-03-10 01:41:49 -0500

    ci: add manpage code, migrate to xtask

--- build.rs
+++ build.rs
@@ -1,33 +1,41 @@
 use clap_complete::{generate_to, shells::Shell};
-use std::{env, fs, process};
+use std::{env, fs, io::Result, path::PathBuf, process};
 
 include!("src/clap.rs");
 
-fn main() {
-    // OUT_DIR is where extra build files are written to for Cargo.
-    let out_dir = match env::var_os("OUT_DIR") {
-        Some(out_dir) => out_dir,
-        None => {
+fn main() -> Result<()> {
+    if Ok("release".to_owned()) == env::var("PROFILE") {
+        // OUT_DIR is where extra build files are written to for Cargo.
+        let out_dir = if let Some(out_dir) = env::var_os("OUT_DIR") {
+            PathBuf::from(out_dir)
+        } else {
             eprintln!("The OUT_DIR environment variable was not set!  Aborting...");
             process::exit(1)
-        }
-    };
-    match fs::create_dir_all(&out_dir) {
-        Ok(()) => {}
-        Err(err) => {
+        };
+
+        if let Err(err) = fs::create_dir_all(&out_dir) {
             eprintln!(
-                "Failed to create a directory at OUT_DIR location {:?}, encountered error {:?}.  Aborting...",
-                out_dir, err
-            );
+            "Failed to create a directory at OUT_DIR location {:?}, encountered error {:?}.  Aborting...",
+            out_dir, err
+        );
             process::exit(1)
         }
+
+        // Generate completions
+        let mut app = build_app();
+        generate_to(Shell::Bash, &mut app, "btm", &out_dir)?;
+        generate_to(Shell::Zsh, &mut app, "btm", &out_dir)?;
+        generate_to(Shell::Fish, &mut app, "btm", &out_dir)?;
+        generate_to(Shell::PowerShell, &mut app, "btm", &out_dir)?;
+        generate_to(Shell::Elvish, &mut app, "btm", &out_dir)?;
+
+        // Generate manpage
+        // let man = clap_mangen::Man::new(app);
+        // let mut buffer: Vec<u8> = Default::default();
+        // man.render(&mut buffer).unwrap();
+
+        // std::fs::write(out_dir.join("btm.1"), buffer)?;
     }
 
-    // Generate completions
-    let mut app = build_app();
-    generate_to(Shell::Bash, &mut app, "btm", &out_dir).unwrap();
-    generate_to(Shell::Zsh, &mut app, "btm", &out_dir).unwrap();
-    generate_to(Shell::Fish, &mut app, "btm", &out_dir).unwrap();
-    generate_to(Shell::PowerShell, &mut app, "btm", &out_dir).unwrap();
-    generate_to(Shell::Elvish, &mut app, "btm", &out_dir).unwrap();
+    Ok(())
 }
