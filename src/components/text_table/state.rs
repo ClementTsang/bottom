f1e6112e2a00ac8fd862d12883d3100bec2cfd11
Viridi <virididev@gmail.com>
2022-07-30 20:06:41 +0200

    add more 'do nothing' cases to `update_position`

--- src/components/text_table/state.rs
+++ src/components/text_table/state.rs
@@ -472,7 +472,10 @@ impl<H: TableComponentHeader> TableComponentState<H> {
 
     /// Updates the position if possible, and if there is a valid change, returns the new position.
     pub fn update_position(&mut self, change: i64, num_entries: usize) -> Option<usize> {
-        if change == 0 {
+        if change == 0
+            || (change > 0 && self.current_scroll_position == num_entries - 1)
+            || (change < 0 && self.current_scroll_position == 0)
+        {
             return None;
         }
 
