57ef4e3fa059d8f67f06f023b1a58e1bdf9eab80
Justin Martin <jaming@protonmail.com>
2022-10-13 22:58:26 -0400

    use vec for mem basic widget drawing

--- src/canvas/widgets/mem_basic.rs
+++ src/canvas/widgets/mem_basic.rs
@@ -14,9 +14,7 @@ impl Painter {
         &self, f: &mut Frame<'_, B>, app_state: &mut App, draw_loc: Rect, widget_id: u64,
     ) {
         let mem_data = &app_state.converted_data.mem_data;
-        let swap_data = &app_state.converted_data.swap_data;
-        let mut swap_used = false;
-        let mut size = 1;
+        let mut draw_widgets: Vec<PipeGauge<'_>> = Vec::new();
 
         if app_state.current_widget.widget_id == widget_id {
             f.render_widget(
@@ -29,11 +27,6 @@ impl Painter {
 
         let ram_ratio = if let Some(mem) = mem_data.last() {
             mem.1
-        } else {
-            0.0
-        };
-        let swap_ratio = if let Some(swap) = swap_data.last() {
-            swap.1
         } else {
             0.0
         };
@@ -51,57 +44,70 @@ impl Painter {
                 EMPTY_MEMORY_FRAC_STRING.to_string()
             };
 
-        let swap_fraction_label =
-            if let Some((_, label_frac)) = &app_state.converted_data.swap_labels {
-                size += 1;
-                swap_used = true;
-                if app_state.basic_mode_use_percent {
-                    format!("{:3.0}%", swap_ratio.round())
-                } else {
-                    label_frac.trim().to_string()
-                }
+        draw_widgets.push(
+            PipeGauge::default()
+                .ratio(ram_ratio / 100.0)
+                .start_label("RAM")
+                .inner_label(memory_fraction_label)
+                .label_style(self.colours.ram_style)
+                .gauge_style(self.colours.ram_style),
+        );
+
+        let swap_data = &app_state.converted_data.swap_data;
+
+        let swap_ratio = if let Some(swap) = swap_data.last() {
+            swap.1
+        } else {
+            0.0
+        };
+
+        if let Some((_, label_frac)) = &app_state.converted_data.swap_labels {
+            let swap_fraction_label = if app_state.basic_mode_use_percent {
+                format!("{:3.0}%", swap_ratio.round())
             } else {
-                EMPTY_MEMORY_FRAC_STRING.to_string()
+                label_frac.trim().to_string()
             };
+            draw_widgets.push(
+                PipeGauge::default()
+                    .ratio(swap_ratio / 100.0)
+                    .start_label("SWP")
+                    .inner_label(swap_fraction_label)
+                    .label_style(self.colours.swap_style)
+                    .gauge_style(self.colours.swap_style),
+            );
+        }
 
         #[cfg(feature = "zfs")]
-        let (arc_used, arc_fraction_label, arc_ratio) = {
+        {
             let arc_data = &app_state.converted_data.arc_data;
             let arc_ratio = if let Some(arc) = arc_data.last() {
                 arc.1
             } else {
                 0.0
             };
-            let mut arc_used = false;
-            let arc_label = {
-                if let Some((_, label_frac)) = &app_state.converted_data.arc_labels {
-                    size += 1;
-                    arc_used = true;
-                    if app_state.basic_mode_use_percent {
-                        format!("{:3.0}%", arc_ratio.round())
-                    } else {
-                        label_frac.trim().to_string()
-                    }
+            if let Some((_, label_frac)) = &app_state.converted_data.arc_labels {
+                let arc_fraction_label = if app_state.basic_mode_use_percent {
+                    format!("{:3.0}%", arc_ratio.round())
                 } else {
-                    EMPTY_MEMORY_FRAC_STRING.to_string()
-                }
-            };
-            (arc_used, arc_label, arc_ratio)
-        };
+                    label_frac.trim().to_string()
+                };
+                draw_widgets.push(
+                    PipeGauge::default()
+                        .ratio(arc_ratio / 100.0)
+                        .start_label("ARC")
+                        .inner_label(arc_fraction_label)
+                        .label_style(self.colours.arc_style)
+                        .gauge_style(self.colours.arc_style),
+                );
+            }
+        }
 
         #[cfg(feature = "gpu")]
-        let (gpu_used, gpu_fraction_labels) = {
-            let mut gpu_used = false;
-            let mut gpu_labels = match &app_state.converted_data.gpu_data {
-                Some(data) => Vec::with_capacity(data.len()),
-                None => Vec::with_capacity(0),
-            };
+        {
             let gpu_styles = &self.colours.gpu_colour_styles;
             let mut color_index = 0;
             if let Some(gpu_data) = &app_state.converted_data.gpu_data {
                 gpu_data.iter().for_each(|gpu_data_vec| {
-                    size += 1;
-                    gpu_used = true;
                     let gpu_data = gpu_data_vec.points.as_slice();
                     let gpu_ratio = if let Some(gpu) = gpu_data.last() {
                         gpu.1
@@ -127,7 +133,7 @@ impl Painter {
                             gpu_styles[color_index - 1]
                         }
                     };
-                    gpu_labels.push(
+                    draw_widgets.push(
                         PipeGauge::default()
                             .ratio(gpu_ratio / 100.0)
                             .start_label("GPU")
@@ -136,67 +142,22 @@ impl Painter {
                             .gauge_style(style),
                     );
                 });
-                (gpu_used, gpu_labels)
-            } else {
-                (false, vec![])
             }
-        };
+        }
 
         let constraint_layout: Vec<Constraint> = std::iter::repeat(Constraint::Length(1))
-            .take(size)
+            .take(draw_widgets.len())
             .collect();
+
         let margined_loc = Layout::default()
             .constraints(constraint_layout)
             .direction(Direction::Vertical)
             .horizontal_margin(1)
             .split(draw_loc);
 
-        let mut draw_index = 0;
-
-        f.render_widget(
-            PipeGauge::default()
-                .ratio(ram_ratio / 100.0)
-                .start_label("RAM")
-                .inner_label(memory_fraction_label)
-                .label_style(self.colours.ram_style)
-                .gauge_style(self.colours.ram_style),
-            margined_loc[draw_index],
-        );
-
-        if swap_used {
-            draw_index += 1;
-            f.render_widget(
-                PipeGauge::default()
-                    .ratio(swap_ratio / 100.0)
-                    .start_label("SWP")
-                    .inner_label(swap_fraction_label)
-                    .label_style(self.colours.swap_style)
-                    .gauge_style(self.colours.swap_style),
-                margined_loc[draw_index],
-            );
-        }
-
-        #[cfg(feature = "zfs")]
-        if arc_used {
-            draw_index += 1;
-            f.render_widget(
-                PipeGauge::default()
-                    .ratio(arc_ratio / 100.0)
-                    .start_label("ARC")
-                    .inner_label(arc_fraction_label)
-                    .label_style(self.colours.arc_style)
-                    .gauge_style(self.colours.arc_style),
-                margined_loc[draw_index],
-            );
-        }
-
-        #[cfg(feature = "gpu")]
-        if gpu_used {
-            for item in gpu_fraction_labels {
-                draw_index += 1;
-                f.render_widget(item, margined_loc[draw_index]);
-            }
-        }
+        draw_widgets.iter().enumerate().for_each(|(index, widget)| {
+            f.render_widget(widget.to_owned(), margined_loc[index]);
+        });
 
         // Update draw loc in widget map
         if app_state.should_get_widget_bounds() {
