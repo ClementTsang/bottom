69b0d102a65560fa0ba4be023cfe1a2f86882663
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2022-10-16 03:15:08 -0400

    refactor: remove redundant scroll direction enum
    This was made redundant from the table refactor.

--- src/app/states.rs
+++ src/app/states.rs
@@ -13,20 +13,6 @@ use super::widgets::{
 };
 
 #[derive(Debug)]
-pub enum ScrollDirection {
-    // UP means scrolling up --- this usually DECREMENTS
-    Up,
-    // DOWN means scrolling down --- this usually INCREMENTS
-    Down,
-}
-
-impl Default for ScrollDirection {
-    fn default() -> Self {
-        ScrollDirection::Down
-    }
-}
-
-#[derive(Debug)]
 pub enum CursorDirection {
     Left,
     Right,
