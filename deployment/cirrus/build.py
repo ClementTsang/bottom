035de3f0bcc2e5b98ce670e6c1d6c9d087f5eda3
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2022-10-27 07:03:11 -0400

    update comment

--- deployment/cirrus/build.py
+++ deployment/cirrus/build.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 
 # A simple script to trigger Cirrus CI builds and get the release artifacts through Cirrus CI's GraphQL
-# interface. Expects the Cirrus CI API key to be set in CIRRUS_KEY.
+# interface.
+#
+# Expects the Cirrus CI API key to be set in CIRRUS_KEY.
 
 import os
 import json
