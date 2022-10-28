fec56372bb804931dca3c5a8611e7e24f6774c19
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2022-10-27 22:00:37 -0400

    ci: use one skip check for CI workflow (#855)
    Use one single skip check job for CI like other workflows.

--- deployment/cirrus/build.py
+++ deployment/cirrus/build.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 
-# A simple script to trigger Cirrus CI builds and get the release artifacts through Cirrus CI's GraphQL interface.
-# Expects the API key to be set in CIRRUS_KEY.
+# A simple script to trigger Cirrus CI builds and download the release artifacts
+# through Cirrus CI's GraphQL interface.
+#
+# Expects the Cirrus CI API key to be set in the CIRRUS_KEY environment variable.
 
 import os
 import json
