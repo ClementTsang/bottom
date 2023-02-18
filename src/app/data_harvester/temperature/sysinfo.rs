0aef56dcb36c298512290db9c4d4fa836bf9c083
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2023-02-18 18:30:24 -0500

    other: support hw.temperature-based temps for FreeBSD

--- src/app/data_harvester/temperature/sysinfo.rs
+++ src/app/data_harvester/temperature/sysinfo.rs
@@ -38,5 +38,30 @@ pub fn get_temperature_data(
         super::nvidia::add_nvidia_data(&mut temperature_vec, temp_type, filter)?;
     }
 
+    // For RockPro64 boards on FreeBSD, they apparently use "hw.temperature" for sensors.
+    #[cfg(target_os = "freebsd")]
+    {
+        use sysctl::Sysctl;
+
+        const KEY: &str = "hw.temperature";
+        if let Ok(root) = sysctl::Ctl::new(KEY) {
+            for ctl in sysctl::CtlIter::below(root).flatten() {
+                if let (Ok(name), Ok(temp)) = (ctl.name(), ctl.value()) {
+                    if let Some(temp) = temp.as_temperature() {
+                        temperature_vec.push(TempHarvest {
+                            name,
+                            temperature: match temp_type {
+                                TemperatureType::Celsius => temp.celsius(),
+                                TemperatureType::Kelvin => temp.kelvin(),
+                                TemperatureType::Fahrenheit => temp.fahrenheit(),
+                            },
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    // TODO: Should we instead use a hashmap -> vec to skip dupes?
     Ok(Some(temperature_vec))
 }
