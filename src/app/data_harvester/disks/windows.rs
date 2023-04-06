eff2f16815e19e81130461a0a7cbb4e22d1e8fb0
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2023-04-06 01:15:39 -0400

    bleh

--- src/app/data_harvester/disks/windows.rs
+++ src/app/data_harvester/disks/windows.rs
@@ -36,16 +36,16 @@ pub(crate) fn get_disk_usage(
                 .unwrap_or_else(|_| "Mount unavailable".to_string());
 
             if keep_disk_entry(&name, &mount_point, disk_filter, mount_filter) {
-                let free_space = Some(disk.available_space());
-                let total_space = Some(disk.total_space());
-                let used_space = Some(total_space - free_space);
+                let free_space = disk.available_space();
+                let total_space = disk.total_space();
+                let used_space = total_space - free_space;
 
                 Some(DiskHarvest {
                     name,
                     mount_point,
-                    free_space,
-                    used_space,
-                    total_space,
+                    free_space: Some(free_space),
+                    used_space: Some(used_space),
+                    total_space: Some(total_space),
                 })
             } else {
                 None
