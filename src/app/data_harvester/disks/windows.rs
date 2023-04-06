c396659a9516990e85cac4a350748baad4edcbb1
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2023-04-06 01:08:45 -0400

    fix some typing

--- src/app/data_harvester/disks/windows.rs
+++ src/app/data_harvester/disks/windows.rs
@@ -2,7 +2,7 @@
 
 use sysinfo::{DiskExt, System, SystemExt};
 
-use super::{keep_disk_entry, DiskHarvest, IoData, IoHarvest};
+use super::{keep_disk_entry, DiskHarvest, IoHarvest};
 use crate::{app::filter::Filter, utils::error};
 
 pub fn get_io_usage() -> error::Result<IoHarvest> {
@@ -36,9 +36,9 @@ pub(crate) fn get_disk_usage(
                 .unwrap_or_else(|_| "Mount unavailable".to_string());
 
             if keep_disk_entry(&name, &mount_point, disk_filter, mount_filter) {
-                let free_space = disk.available_space();
-                let total_space = disk.total_space();
-                let used_space = total_space - free_space;
+                let free_space = Some(disk.available_space());
+                let total_space = Some(disk.total_space());
+                let used_space = Some(total_space - free_space);
 
                 Some(DiskHarvest {
                     name,
