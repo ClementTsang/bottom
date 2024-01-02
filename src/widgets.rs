9efe60c66f6acb5ab0fa12fbf909e52d4d470ad3
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2024-01-01 22:25:35 -0500

    rename battery info widget file

--- src/widgets.rs
+++ src/widgets.rs
@@ -1,4 +1,4 @@
-pub mod battery_widget;
+pub mod battery_info;
 pub mod cpu_graph;
 pub mod disk_table;
 pub mod mem_graph;
@@ -6,7 +6,7 @@ pub mod net_graph;
 pub mod process_table;
 pub mod temperature_table;
 
-pub use battery_widget::*;
+pub use battery_info::*;
 pub use cpu_graph::*;
 pub use disk_table::*;
 pub use mem_graph::*;
--- src/widgets/battery_widget.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Default)]
-pub struct BatteryWidgetState {
-    pub currently_selected_battery_index: usize,
-    pub tab_click_locs: Option<Vec<((u16, u16), (u16, u16))>>,
-}
