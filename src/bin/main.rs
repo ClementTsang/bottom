bef4292b39b30b12b04c31755d2b7ec381540d91
GrecuAlexandru <alexandru.grecu27@gmail.com>
2024-01-08 11:05:02 +0200

    ran cargo fmt locally

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -277,17 +277,17 @@ fn main() -> Result<()> {
 
                             app.converted_data.mem_labels = convert_mem_label(
                                 &app.data_collection.memory_harvest,
-                                app.app_config_fields.memory_use_mega_prefix
+                                app.app_config_fields.memory_use_mega_prefix,
                             );
                             app.converted_data.swap_labels = convert_mem_label(
                                 &app.data_collection.swap_harvest,
-                                app.app_config_fields.memory_use_mega_prefix
+                                app.app_config_fields.memory_use_mega_prefix,
                             );
                             #[cfg(not(target_os = "windows"))]
                             {
                                 app.converted_data.cache_labels = convert_mem_label(
                                     &app.data_collection.cache_harvest,
-                                    app.app_config_fields.memory_use_mega_prefix
+                                    app.app_config_fields.memory_use_mega_prefix,
                                 );
                             }
 
@@ -295,7 +295,7 @@ fn main() -> Result<()> {
                             {
                                 let arc_labels = convert_mem_label(
                                     &app.data_collection.arc_harvest,
-                                    app.app_config_fields.memory_use_mega_prefix
+                                    app.app_config_fields.memory_use_mega_prefix,
                                 );
                                 app.converted_data.arc_labels = arc_labels;
                             }
