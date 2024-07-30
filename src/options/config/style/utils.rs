cc8937c12abdde8c0af13e73c224c59479e9cd5a
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2024-07-30 02:56:24 -0400

    tests

--- src/options/config/style/utils.rs
+++ src/options/config/style/utils.rs
@@ -137,11 +137,11 @@ macro_rules! set_style {
                     $palette_field = $palette_field.fg(
                         crate::options::config::style::utils::str_to_colour(&colour.0)
                             .map_err(|err| match stringify!($config_location).split_once(".") {
-                                Some((_, loc)) => OptionError::config(format!(
+                                Some((_, loc)) => crate::options::OptionError::config(format!(
                                     "Please update 'styles.{loc}.{}' in your config file. {err}",
                                     stringify!($field)
                                 )),
-                                None => OptionError::config(format!(
+                                None => crate::options::OptionError::config(format!(
                                     "Please update 'styles.{}' in your config file. {err}",
                                     stringify!($field)
                                 )),
@@ -153,11 +153,11 @@ macro_rules! set_style {
                         $palette_field = $palette_field.fg(
                             crate::options::config::style::utils::str_to_colour(&fg.0)
                                 .map_err(|err| match stringify!($config_location).split_once(".") {
-                                    Some((_, loc)) => OptionError::config(format!(
+                                    Some((_, loc)) => crate::options::OptionError::config(format!(
                                         "Please update 'styles.{loc}.{}' in your config file. {err}",
                                         stringify!($field)
                                     )),
-                                    None => OptionError::config(format!(
+                                    None => crate::options::OptionError::config(format!(
                                         "Please update 'styles.{}' in your config file. {err}",
                                         stringify!($field)
                                     )),
@@ -169,11 +169,11 @@ macro_rules! set_style {
                         $palette_field = $palette_field.bg(
                             crate::options::config::style::utils::str_to_colour(&bg.0)
                                 .map_err(|err| match stringify!($config_location).split_once(".") {
-                                    Some((_, loc)) => OptionError::config(format!(
+                                    Some((_, loc)) => crate::options::OptionError::config(format!(
                                         "Please update 'styles.{loc}.{}' in your config file. {err}",
                                         stringify!($field)
                                     )),
-                                    None => OptionError::config(format!(
+                                    None => crate::options::OptionError::config(format!(
                                         "Please update 'styles.{}' in your config file. {err}",
                                         stringify!($field)
                                     )),
@@ -192,11 +192,11 @@ macro_rules! set_colour {
             $palette_field = $palette_field.fg(
                 crate::options::config::style::utils::str_to_colour(&colour.0).map_err(|err| {
                     match stringify!($config_location).split_once(".") {
-                        Some((_, loc)) => OptionError::config(format!(
+                        Some((_, loc)) => crate::options::OptionError::config(format!(
                             "Please update 'styles.{loc}.{}' in your config file. {err}",
                             stringify!($field)
                         )),
-                        None => OptionError::config(format!(
+                        None => crate::options::OptionError::config(format!(
                             "Please update 'styles.{}' in your config file. {err}",
                             stringify!($field)
                         )),
@@ -235,6 +235,10 @@ pub(super) use {opt, set_colour, set_colour_list, set_style};
 
 #[cfg(test)]
 mod test {
+
+    use tui::style::Style;
+
+    use crate::options::config::style::{ColorStr, TextStyleConfig};
 
     use super::*;
 
@@ -387,5 +391,108 @@ mod test {
         assert!(convert_rgb_to_color("1, -1, 1").is_err());
         assert!(convert_rgb_to_color("1, -100000, 1").is_err());
         assert!(convert_rgb_to_color("1, -100000, 100000").is_err());
+    }
+
+    struct DummyConfig {
+        inner: Option<InnerDummyConfig>,
+    }
+
+    struct InnerDummyConfig {
+        color_a: Option<ColorStr>,
+        color_b: Option<ColorStr>,
+        color_c: Option<ColorStr>,
+        color_d: Option<ColorStr>,
+        text_a: Option<TextStyleConfig>,
+        text_b: Option<TextStyleConfig>,
+        text_c: Option<TextStyleConfig>,
+        text_d: Option<TextStyleConfig>,
+        text_e: Option<TextStyleConfig>,
+    }
+
+    impl Default for InnerDummyConfig {
+        fn default() -> Self {
+            Self {
+                color_a: None,
+                color_b: Some(ColorStr("red".into())),
+                color_c: Some(ColorStr("255, 255, 255".into())),
+                color_d: Some(ColorStr("#000000".into())),
+                text_a: Some(TextStyleConfig::Colour(ColorStr("green".into()))),
+                text_b: Some(TextStyleConfig::TextStyle {
+                    color: None,
+                    bg_color: None,
+                    bold: None,
+                }),
+                text_c: Some(TextStyleConfig::TextStyle {
+                    color: Some(ColorStr("magenta".into())),
+                    bg_color: Some(ColorStr("255, 255, 255".into())),
+                    bold: Some(false),
+                }),
+                text_d: Some(TextStyleConfig::TextStyle {
+                    color: Some(ColorStr("#fff".into())),
+                    bg_color: Some(ColorStr("1, 1, 1".into())),
+                    bold: Some(true),
+                }),
+                text_e: None,
+            }
+        }
+    }
+
+    #[test]
+    fn test_set_colour() -> anyhow::Result<()> {
+        let mut s = Style::default().fg(Color::Black);
+        let dummy = DummyConfig {
+            inner: Some(InnerDummyConfig::default()),
+        };
+
+        set_colour!(s, &dummy.inner, color_a);
+        assert_eq!(s.fg.unwrap(), Color::Black);
+        assert_eq!(s.bg, None);
+
+        set_colour!(s, &dummy.inner, color_b);
+        assert_eq!(s.fg.unwrap(), Color::Red);
+        assert_eq!(s.bg, None);
+
+        set_colour!(s, &dummy.inner, color_c);
+        assert_eq!(s.fg.unwrap(), Color::Rgb(255, 255, 255));
+        assert_eq!(s.bg, None);
+
+        set_colour!(s, &dummy.inner, color_d);
+        assert_eq!(s.fg.unwrap(), Color::Rgb(0, 0, 0));
+        assert_eq!(s.bg, None);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_set_style() -> anyhow::Result<()> {
+        let mut s = Style::default().fg(Color::Black);
+        let dummy = DummyConfig {
+            inner: Some(InnerDummyConfig::default()),
+        };
+
+        set_style!(s, &dummy.inner, text_e);
+        assert_eq!(s.fg.unwrap(), Color::Black);
+        assert_eq!(s.bg, None);
+        assert!(s.add_modifier.is_empty());
+
+        set_style!(s, &dummy.inner, text_a);
+        assert_eq!(s.fg.unwrap(), Color::Green);
+        assert_eq!(s.bg, None);
+
+        set_style!(s, &dummy.inner, text_b);
+        assert_eq!(s.fg.unwrap(), Color::Green);
+        assert_eq!(s.bg, None);
+
+        set_style!(s, &dummy.inner, text_c);
+        assert_eq!(s.fg.unwrap(), Color::Magenta);
+        assert_eq!(s.bg.unwrap(), Color::Rgb(255, 255, 255));
+
+        set_style!(s, &dummy.inner, text_d);
+        assert_eq!(s.fg.unwrap(), Color::Rgb(255, 255, 255));
+        assert_eq!(s.bg.unwrap(), Color::Rgb(1, 1, 1));
+        // TODO: Add this
+        // assert!(s.add_modifier.contains(Modifier::BOLD));
+
+        Ok(())
     }
 }
