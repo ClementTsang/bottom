0f501bf62e5cc765490b5bda9edb28a73b427d93
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2024-09-02 22:11:47 -0400

    allow boxed local

--- src/app/data_farmer.rs
+++ src/app/data_farmer.rs
@@ -206,6 +206,8 @@ impl DataCollection {
         self.timed_data_vec.shrink_to_fit();
     }
 
+    // Clippy allow to avoid warning on certain platforms (e.g. 32-bit).
+    #[allow(clippy::boxed_local)]
     pub fn eat_data(&mut self, harvested_data: Box<Data>) {
         let harvested_time = harvested_data.collection_time;
         let mut new_entry = TimedData::default();
