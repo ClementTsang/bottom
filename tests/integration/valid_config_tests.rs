4e47f9b51a57a16d406be8ce0da6e71ec246d100
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2024-09-12 05:51:23 -0400

    bug: fix incorrect default config definitions for chart legends (#1594)
    I had changed how this was parsed in-code but I forgot to update the default configs. This also adds some e2e tests to hopefully catch this all for real in the future, since the schema ones don't catch this stuff and the constants test doesn't actually run the binary for a proper e2e test.

--- tests/integration/valid_config_tests.rs
+++ tests/integration/valid_config_tests.rs
@@ -2,6 +2,9 @@
 
 use std::{io::Read, thread, time::Duration};
 
+#[cfg(feature = "default")]
+use std::{io::Write, path::Path};
+
 use crate::util::spawn_btm_in_pty;
 
 fn reader_to_string(mut reader: Box<dyn Read>) -> String {
@@ -51,6 +54,101 @@ fn test_bad_basic() {
 #[test]
 fn test_empty() {
     run_and_kill(&["-C", "./tests/valid_configs/empty_config.toml"]);
+}
+
+#[cfg(feature = "default")]
+fn test_uncommented_default_config(original: &Path, test_name: &str) {
+    use regex::Regex;
+
+    // Take the default config file and uncomment everything.
+    let default_config = match std::fs::File::open(original) {
+        Ok(mut default_config_file) => {
+            let mut buf = String::new();
+            default_config_file
+                .read_to_string(&mut buf)
+                .expect("can read file");
+
+            buf
+        }
+        Err(err) => {
+            println!("Could not open default config, skipping {test_name}. Error: {err:?}");
+            return;
+        }
+    };
+
+    let default_config = Regex::new(r"(?m)^#([a-zA-Z\[])")
+        .unwrap()
+        .replace_all(&default_config, "$1");
+
+    let default_config = Regex::new(r"(?m)^#(\s\s+)([a-zA-Z\[])")
+        .unwrap()
+        .replace_all(&default_config, "$2");
+
+    let mut uncommented_config = match tempfile::NamedTempFile::new() {
+        Ok(tf) => tf,
+        Err(err) => {
+            println!("Could not create a temp file, skipping {test_name}. Error: {err:?}");
+            return;
+        }
+    };
+
+    if let Err(err) = uncommented_config.write_all(default_config.as_bytes()) {
+        println!("Could not write to temp file, skipping {test_name}. Error: {err:?}");
+        return;
+    }
+
+    run_and_kill(&["-C", &uncommented_config.path().to_string_lossy()]);
+
+    uncommented_config.close().unwrap();
+}
+
+#[cfg(feature = "default")]
+#[test]
+fn test_default() {
+    test_uncommented_default_config(
+        Path::new("./sample_configs/default_config.toml"),
+        "test_default",
+    );
+}
+
+#[cfg(feature = "default")]
+#[test]
+fn test_new_default() {
+    use tempfile::TempPath;
+
+    let new_temp_default_path = match tempfile::NamedTempFile::new() {
+        Ok(temp_file) => temp_file.into_temp_path(),
+        Err(err) => {
+            println!("Could not create a temp file, skipping test_new_default. Error: {err:?}");
+            return;
+        }
+    };
+
+    // This is a hack because we need a temp file that doesn't exist.
+    let actual_temp_default_path = new_temp_default_path.to_path_buf();
+    new_temp_default_path.close().unwrap();
+
+    if !actual_temp_default_path.exists() {
+        run_and_kill(&["-C", &(actual_temp_default_path.to_string_lossy())]);
+
+        // Re-take control over the temp path to ensure it gets deleted.
+        let actual_temp_default_path = TempPath::from_path(actual_temp_default_path);
+        test_uncommented_default_config(&actual_temp_default_path, "test_new_default");
+
+        actual_temp_default_path.close().unwrap();
+    } else {
+        println!("temp path we want to check exists, skip test_new_default test.");
+    }
+}
+
+#[test]
+fn test_demo() {
+    let path: &str = "./sample_configs/demo_config.toml";
+    if std::path::Path::new(path).exists() {
+        run_and_kill(&["-C", path]);
+    } else {
+        println!("Could not read demo config.");
+    }
 }
 
 #[test]
