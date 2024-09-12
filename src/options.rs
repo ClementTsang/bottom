f3ce9b572daeb71a938092edfaa4b83a84c3bf88
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2024-09-12 04:30:21 -0400

    forgot memory oops

--- src/options.rs
+++ src/options.rs
@@ -984,8 +984,11 @@ fn get_memory_legend_position(
             position => Some(parse_config_value!(position.parse(), "memory_legend")?),
         }
     } else if let Some(flags) = &config.flags {
-        if let Some(legend) = &flags.memory_legend {
-            Some(parse_arg_value!(legend.parse(), "memory_legend")?)
+        if let Some(s) = &flags.memory_legend {
+            match s.to_ascii_lowercase().trim() {
+                "none" => None,
+                position => Some(parse_arg_value!(position.parse(), "memory_legend")?),
+            }
         } else {
             Some(LegendPosition::default())
         }
