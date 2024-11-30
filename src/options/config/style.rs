b43f573e82d29cd8452279d4fe2a8dc4b0f57b63
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2024-11-30 01:57:42 -0500

    add widget border type

--- src/options/config/style.rs
+++ src/options/config/style.rs
@@ -219,6 +219,36 @@ impl ColourPalette {
     }
 }
 
+#[derive(Default, Clone, Debug, Serialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
+pub(crate) enum WidgetBorderType {
+    #[default]
+    Default,
+    Rounded,
+    Double,
+    Thick,
+    None,
+}
+
+impl<'de> Deserialize<'de> for WidgetBorderType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?.to_lowercase();
+        match value.as_str() {
+            "default" => Ok(WidgetBorderType::Default),
+            "rounded" => Ok(WidgetBorderType::Rounded),
+            "double" => Ok(WidgetBorderType::Double),
+            "thick" => Ok(WidgetBorderType::Thick),
+            "none" => Ok(WidgetBorderType::None),
+            _ => Err(serde::de::Error::custom(
+                "doesn't match any widget border type",
+            )),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
 
--- src/options/config/style/widgets.rs
+++ src/options/config/style/widgets.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use super::{ColorStr, TextStyleConfig};
+use super::{ColorStr, TextStyleConfig, WidgetBorderType};
 
 /// General styling for generic widgets.
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
@@ -26,4 +26,7 @@ pub(crate) struct WidgetStyle {
 
     /// Text styling for text when representing something that is disabled.
     pub(crate) disabled_text: Option<TextStyleConfig>,
+
+    /// Widget borders styling.
+    pub(crate) widget_borders: Option<WidgetBorderType>,
 }
