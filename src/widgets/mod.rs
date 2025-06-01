e35d0012845a7187af36cf56fb51a1beae5adf8a
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2025-06-01 00:57:18 -0400

    some refactoring and docs stuff

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,17 +1,21 @@
-pub mod battery_info;
-pub mod cpu_graph;
-pub mod disk_graph;
-pub mod disk_table;
-pub mod mem_graph;
-pub mod network_graph;
-pub mod process_table;
-pub mod temp_graph;
-pub mod temperature_table;
+//! The "widgets" of bottom.
+
+mod battery_info;
+mod cpu_graph;
+mod disk_graph;
+mod disk_table;
+mod mem_graph;
+mod network_graph;
+mod process_table;
+mod temperature_graph;
+mod temperature_table;
 
 pub use battery_info::*;
 pub use cpu_graph::*;
+pub use disk_graph::*;
 pub use disk_table::*;
 pub use mem_graph::*;
 pub use network_graph::*;
 pub use process_table::*;
+pub use temperature_graph::*;
 pub use temperature_table::*;
