164ba0780c44581b738a5ffa667043a131b200bf
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2025-05-31 22:55:50 -0400

    add files

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,9 +1,11 @@
 pub mod battery_info;
 pub mod cpu_graph;
+pub mod disk_graph;
 pub mod disk_table;
 pub mod mem_graph;
 pub mod network_graph;
 pub mod process_table;
+pub mod temp_graph;
 pub mod temperature_table;
 
 pub use battery_info::*;
---
+++ src/widgets/temp_graph.rs
@@ -0,0 +1,1 @@
+//! Code for a temperature graph widget.
