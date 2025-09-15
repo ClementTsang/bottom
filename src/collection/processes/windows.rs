5e844b7ef1e0f77e77f6a09dba3e4a7ac79cc7b1
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2025-09-14 20:53:33 -0400

    hmmm hopefully that works?

--- src/collection/processes/windows.rs
+++ src/collection/processes/windows.rs
@@ -108,8 +108,7 @@ pub fn sysinfo_process_data(
             process_state,
             user: process_val
                 .user_id()
-                .and_then(|uid| users.get_user_by_id(uid))
-                .map_or_else(|| "N/A".into(), |user| user.name().to_owned().into()),
+                .and_then(|uid| users.get_user_by_id(uid).into()),
             time: if process_val.start_time() == 0 {
                 // Workaround for sysinfo occasionally returning a start time equal to UNIX
                 // epoch, giving a run time in the range of 50+ years. We just
