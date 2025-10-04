17751a755ae2491d376263987989aadfcb8d735c
Saphereye <adarshdas950@gmail.com>
2025-10-04 15:25:45 +0530

    refactor: moved the if statements inside the match case

--- src/event.rs
+++ src/event.rs
@@ -56,25 +56,15 @@ pub fn handle_key_event_or_break(
     // c_debug!("KeyEvent: {event:?}");
 
     if event.modifiers.is_empty() {
-        // Required catch for searching - otherwise you couldn't search with q.
-        if event.code == KeyCode::Char('q') && !app.is_in_search_widget() {
-            return true;
-        }
-
-        // Handle spacebar.
-        // Without the search check, you couldn’t search for names with spaces.
-        if event.code == KeyCode::Char(' ') && !app.is_in_search_widget() {
-            app.toggle_tree_mode();
-            return false;
-        }
-
         match event.code {
+            KeyCode::Char('q') if !app.is_in_search_widget() => return true,
             KeyCode::End => app.skip_to_last(),
             KeyCode::Home => app.skip_to_first(),
             KeyCode::Up => app.on_up_key(),
             KeyCode::Down => app.on_down_key(),
             KeyCode::Left => app.on_left_key(),
             KeyCode::Right => app.on_right_key(),
+            KeyCode::Char(' ') if !app.is_in_search_widget() => app.toggle_tree_mode(),
             KeyCode::Char(caught_char) => app.on_char_key(caught_char),
             KeyCode::Esc => app.on_esc(),
             KeyCode::Enter => app.on_enter(),
