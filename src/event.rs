53986e5254ae6f40f3734121219dd9d06d49bdbe
Saphereye <adarshdas950@gmail.com>
2025-10-13 13:53:44 +0530

    revert: changes

--- src/event.rs
+++ src/event.rs
@@ -64,7 +64,6 @@ pub fn handle_key_event_or_break(
             KeyCode::Down => app.on_down_key(),
             KeyCode::Left => app.on_left_key(),
             KeyCode::Right => app.on_right_key(),
-            KeyCode::Char(' ') if !app.is_in_search_widget() => app.toggle_tree_mode(),
             KeyCode::Char(caught_char) => app.on_char_key(caught_char),
             KeyCode::Esc => app.on_esc(),
             KeyCode::Enter => app.on_enter(),
