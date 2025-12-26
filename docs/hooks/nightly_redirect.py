c71e8f477644d0523cb583cd8b969e5d42e2b28f
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2025-12-25 19:08:45 -0500

    fix for potential time delay?

--- docs/hooks/nightly_redirect.py
+++ docs/hooks/nightly_redirect.py
@@ -1,3 +1,4 @@
+import os
 import mkdocs.plugins
 import urllib.request
 import json
@@ -9,19 +10,28 @@ import json
 def on_config(config):
     print("Running nightly release redirect hook...")
     try:
-        with urllib.request.urlopen("https://api.github.com/repos/ClementTsang/bottom/releases") as response:
-            raw_data = response.read()
-            data = json.loads(raw_data.decode('utf-8'))
+        nightly_tag_name = None
+        override = os.environ.get('MKDOCS_NIGHTLY_RELEASE_OVERRIDE')
 
-            first_nightly = next(release for release in data if "nightly-" in release["tag_name"])
-            nightly_tag_name = first_nightly["tag_name"]
+        if override:
+            nightly_tag_name = override
+        else:
+            with urllib.request.urlopen("https://api.github.com/repos/ClementTsang/bottom/releases") as response:
+                raw_data = response.read()
+                data = json.loads(raw_data.decode('utf-8'))
 
-            redirect_plugin = config.get('plugins', {}).get('redirects')
-            redirects = redirect_plugin.config.get('redirect_maps', {})
+                first_nightly = next(release for release in data if "nightly-" in release["tag_name"])
+                nightly_tag_name = first_nightly["tag_name"]
 
+                redirect_plugin = config.get('plugins', {}).get('redirects')
+                redirects = redirect_plugin.config.get('redirect_maps', {})
+
+        if nightly_tag_name is not None:
             nightly_release_url = f"https://github.com/ClementTsang/bottom/releases/tag/{nightly_tag_name}"
 
             print(f"Updated nightly release redirect to point to {nightly_release_url}")
             redirects["nightly-release.md"] = nightly_release_url
+        else:
+            print("nightly tag name was not set by any means.")
     except Exception as e:
         print(f"error adjusting redirect, falling back to general releases page: {e}")
