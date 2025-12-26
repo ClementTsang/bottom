26bdd54a7aa5025f0a0719e7d4480b07f7c4133b
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2025-12-26 00:54:16 -0500

    fix spacing

--- docs/hooks/nightly_banner.py
+++ docs/hooks/nightly_banner.py
@@ -6,6 +6,8 @@ import mkdocs.plugins
 
 @mkdocs.plugins.event_priority(-100)
 def on_config(config):
+    print("Running nightly banner hook...", file=sys.stderr)
+
     version = os.environ.get("MIKE_DOCS_VERSION")
     print(f"Version: {version}", file=sys.stderr)
 
