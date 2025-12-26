d4ef3942a4d6fa261d50f490f1c11e1efd7671c2
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2025-12-26 00:54:56 -0500

    some more comments

--- docs/hooks/nightly_banner.py
+++ docs/hooks/nightly_banner.py
@@ -8,6 +8,7 @@ import mkdocs.plugins
 def on_config(config):
     print("Running nightly banner hook...", file=sys.stderr)
 
+    # From https://github.com/jimporter/mike/blob/3351d5feabff8ee107f4ad6d1f86055843c7dbf1/mike/mkdocs_utils.py#L13
     version = os.environ.get("MIKE_DOCS_VERSION")
     print(f"Version: {version}", file=sys.stderr)
 
