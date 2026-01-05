b3445b3044a94b7fa2c5bdb6c751a4c02870bb12
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2026-01-05 03:51:43 -0500

    refactor: clean up some query code (#1949)
    * tmp
    
    * intermediate work
    
    * delete some code
    
    * hook things up
    
    * use QueryOptions everywhere instead
    
    * comments
    
    * more cleanup
    
    * even more cleanup
    
    * fmt
    
    * update comment
    
    * some more tests while I'm at it
    
    * add todo for additional tests
    
    * more empty quotes
    
    * more empty quote testing
    
    * even more tests
    
    * driveby test for arg tests

--- src/widgets/process_table/query/and.rs
+++ src/widgets/process_table/query/and.rs
@@ -1,39 +1,22 @@
-use std::fmt::{Debug, Formatter};
+use std::collections::VecDeque;
 
 use crate::{
     collection::processes::ProcessHarvest,
-    widgets::query::{COMPARISON_LIST, Or, Prefix, QueryProcessor, QueryResult, error::QueryError},
+    widgets::query::{
+        COMPARISON_LIST, Or, Prefix, QueryOptions, QueryProcessor, QueryResult, error::QueryError,
+    },
 };
 
 /// A node where both the left hand side or the right hand side are considered.
 /// Note that the right hand side is optional, as that's how I implemented it a long time ago.
-#[derive(Default)]
+#[derive(Debug)]
 pub(super) struct And {
     pub(super) lhs: Prefix,
+    // TODO: Maybe don't need to box rhs?
     pub(super) rhs: Option<Box<Prefix>>,
 }
 
 impl And {
-    pub(super) fn process_regexes(
-        &mut self, is_searching_whole_word: bool, is_ignoring_case: bool,
-        is_searching_with_regex: bool,
-    ) -> QueryResult<()> {
-        self.lhs.process_regexes(
-            is_searching_whole_word,
-            is_ignoring_case,
-            is_searching_with_regex,
-        )?;
-        if let Some(rhs) = &mut self.rhs {
-            rhs.process_regexes(
-                is_searching_whole_word,
-                is_ignoring_case,
-                is_searching_with_regex,
-            )?;
-        }
-
-        Ok(())
-    }
-
     pub(super) fn check(&self, process: &ProcessHarvest, is_using_command: bool) -> bool {
         if let Some(rhs) = &self.rhs {
             self.lhs.check(process, is_using_command) && rhs.check(process, is_using_command)
@@ -43,23 +26,14 @@ impl And {
     }
 }
 
-impl Debug for And {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self.rhs {
-            Some(rhs) => f.write_fmt(format_args!("({:?} AND {:?})", self.lhs, rhs)),
-            None => f.write_fmt(format_args!("{:?}", self.lhs)),
-        }
-    }
-}
-
 impl QueryProcessor for And {
-    fn process(query: &mut std::collections::VecDeque<String>) -> QueryResult<Self>
+    fn process(query: &mut VecDeque<String>, options: &QueryOptions) -> QueryResult<Self>
     where
         Self: Sized,
     {
         const AND_LIST: [&str; 2] = ["and", "&&"];
 
-        let mut lhs = Prefix::process(query)?;
+        let mut lhs = Prefix::process(query, options)?;
         let mut rhs: Option<Box<Prefix>> = None;
 
         while let Some(queue_top) = query.front() {
@@ -67,19 +41,15 @@ impl QueryProcessor for And {
             if AND_LIST.contains(&current_lowercase.as_str()) {
                 query.pop_front();
 
-                rhs = Some(Box::new(Prefix::process(query)?));
+                rhs = Some(Box::new(Prefix::process(query, options)?));
 
                 if let Some(next_queue_top) = query.front() {
                     if AND_LIST.contains(&next_queue_top.to_lowercase().as_str()) {
                         // Must merge LHS and RHS
-                        lhs = Prefix {
-                            or: Some(Box::new(Or {
-                                lhs: And { lhs, rhs },
-                                rhs: None,
-                            })),
-                            regex_prefix: None,
-                            compare_prefix: None,
-                        };
+                        lhs = Prefix::Or(Box::new(Or {
+                            lhs: And { lhs, rhs },
+                            rhs: None,
+                        }));
                         rhs = None;
                     } else {
                         break;
