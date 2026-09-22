d3cf7da9fe5327d44477d1a2492d9103c5f2a624
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2026-09-21 21:50:15 -0400

    other: make schema script use same target dir
    The best way of doing this is probably something like a workspace... but
    I'm not really in the mood to refactor this into a workspace at the
    moment, so this will have to do :p

--- scripts/schema/nightly.sh
+++ scripts/schema/nightly.sh
@@ -5,4 +5,4 @@ set -e
 cd "$(dirname "$0")";
 cd ../..
 
-cargo run --manifest-path scripts/schema_gen/Cargo.toml > schema/nightly/bottom.json
+CARGO_TARGET_DIR="./target" cargo run --manifest-path scripts/schema_gen/Cargo.toml > schema/nightly/bottom.json
