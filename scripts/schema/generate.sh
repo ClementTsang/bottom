1426cbbaa060b4f25d7ae2a4b7896e5e0a251a29
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2026-09-21 21:52:14 -0400

    other: make schema script use same target dir (#2259)
    The best way of doing this is probably something like a workspace... but I'm not really in the mood to refactor this into a workspace at the moment, so this will have to do :p

--- scripts/schema/generate.sh
+++ scripts/schema/generate.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")";
 cd ../..
 
 mkdir -p schema/v$1
-cargo run --manifest-path scripts/schema_gen/Cargo.toml -- $1 > schema/v$1/bottom.json
+CARGO_TARGET_DIR="./target"cargo run --manifest-path scripts/schema_gen/Cargo.toml -- $1 > schema/v$1/bottom.json
